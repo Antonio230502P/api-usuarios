f7ad2c623b612ff91e7a61d9c853d2c1811cb495
Antonio Zarate Rosas <azr230502@gmail.com>
2025-01-17 14:03:48 -0600

    Creación de las tablas Federal State y Roles

--- src/main/java/com/trainibit/first_api/entity/User.java
+++ src/main/java/com/trainibit/first_api/entity/User.java
@@ -2,9 +2,12 @@ package com.trainibit.first_api.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -43,4 +46,8 @@ public class User {
 
     @Column(name = "planet")
     private String planet;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "federal_state_id", nullable = false)
+    private FederalState federalState;
 }
